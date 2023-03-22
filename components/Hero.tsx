c586fec1b97b433936355fe0643e15e5e58754fe
Luis Velazquez <57099991+luis-velazquez@users.noreply.github.com>
2023-03-22 13:20:25 -0500

    fixed images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,12 +4,12 @@ type Props = {};
 
 export default function Hero({}: Props) {
   return (
-    <div className="bg-[url('../public/images/image2.png')] bg-no-repeat bg-left bg-cover bg-fixed">
+    <div className="bg-[url('../public/images/image2.png')] bg-no-repeat bg-left bg-cover sm:bg-fixed">
       <div className="bg-[url('../public/images/border-lines-bottom.png')]  bg-bottom bg-contain sm:bg-cover bg-no-repeat">
         <div className="relative  px-6 pt-14 lg:px-8">
           <div className="mx-auto max-w-2xl pt-32 pb-64  sm:pb-64 lg:pb-96">
             <div className="text-center ">
-              <h1 className="text-5xl font-extrabold tracking-tight text-gray-900 sm:text-6xl dark:text-white  ">
+              <h1 className="text-5xl font-extrabold tracking-tight sm:text-6xl text-white">
                 The Pressure Washing experts at your service!
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-100">
@@ -28,7 +28,7 @@ export default function Hero({}: Props) {
                   href="#"
                   className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-200"
                 >
-                  Learn more <span aria-hidden="true">→</span>
+                  Request quote <span aria-hidden="true">→</span>
                 </a>
               </div>
             </div>
