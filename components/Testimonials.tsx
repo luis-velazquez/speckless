ca27a6f3eb2ae36d792ad5b256366bc12ebcf128
Luis Velazquez <57099991+luis-velazquez@users.noreply.github.com>
2023-03-22 09:49:20 -0500

    commit four

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -48,11 +48,11 @@ export default function Testimonials({}: Props) {
                         </p>
                       </blockquote>
                       <figcaption className="mt-10">
-                        <img
+                        {/* <img
                           className="mx-auto h-10 w-10 rounded-full"
                           src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                           alt=""
-                        />
+                        /> */}
                         <div className="mt-4 flex items-center justify-center space-x-3 text-base">
                           <div className="font-semibold text-gray-900 dark:text-gray-200">
                             Judith Black
